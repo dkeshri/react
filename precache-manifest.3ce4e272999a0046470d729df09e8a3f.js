bb45b464fe1440da6fa45660fb630cb926ce0b60
dkeshri <dkeshridev@gmail.com>
2021-01-06 09:57:09 +0530

    Updates

---
+++ precache-manifest.3ce4e272999a0046470d729df09e8a3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b20829cbf375ad2d0be1ecc37c060bf",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "e2ede96a97de0db7afff",
+    "url": "/React/static/css/main.4002ab72.chunk.css"
+  },
+  {
+    "revision": "e49ce113b02ae4ff78de",
+    "url": "/React/static/js/2.2517bf68.chunk.js"
+  },
+  {
+    "revision": "8ce0251f0cb781d9bf86b53cd85177ca",
+    "url": "/React/static/js/2.2517bf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ede96a97de0db7afff",
+    "url": "/React/static/js/main.61e758af.chunk.js"
+  },
+  {
+    "revision": "7805b880e1d921ab3afb",
+    "url": "/React/static/js/runtime-main.51628f6c.js"
+  },
+  {
+    "revision": "eb4e894d949eb0ce8f21368b70f3dd3b",
+    "url": "/React/static/media/bg.eb4e894d.jpg"
+  },
+  {
+    "revision": "73cbaa0963765a4a93299a3cb664ffb4",
+    "url": "/React/static/media/google.73cbaa09.svg"
+  },
+  {
+    "revision": "fd58a052054c690506118c4b7a0f731d",
+    "url": "/React/static/media/login.fd58a052.png"
+  }
+]);
