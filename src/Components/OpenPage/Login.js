0104364070093925167e339a867d9d88c7aef9c9
dkeshri <dkeshridev@gmail.com>
2021-01-09 18:34:53 +0530

    added drawer functionality

--- src/Components/OpenPage/Login.js
+++ src/Components/OpenPage/Login.js
@@ -1,31 +1,10 @@
 import React from 'react'
-import {
-    Button,
-    Card,
-    CardBody,
-    Row,
-    Col,
-    Form,
-    FormGroup,
-    Input,
-    Label
-} from "reactstrap"
-import googleSvg from "../../assets/img/svg/google.svg"
-import loginImg from "../../assets/img/pages/login.png"
-// import "../../assets/scss/pages/authentication.scss"
+
 function Login() {
     return (
-        <Row className="m-0 justify-content-center">
-            <Col
-                sm="8"
-                xl="7"
-                lg="10"
-                md="8"
-                className="d-flex justify-content-center"
-            >
-               Deepak
-            </Col>
-        </Row>
+       <>
+       login page to implement
+       </>
     )
 }
 
--- src/Router.js
+++ src/Router.js
@@ -3,13 +3,14 @@ import {BrowserRouter as  Router, Switch, Route} from 'react-router-dom'
 import Home from './Components/Home'
 import PageNotFound from './Components/Common/PageNotFound'
 import Login from './Components/OpenPage/Login'
-
+import Dropdown from './Components/Dropdown'
 function AppRouter() {
     return (
         <Router basename="/React">
             <Switch>
                 <Route  exact path="/" component={Home}/>
                 <Route  exact path="/Home" component={Home}/>
+                <Route  exact path="/Dropdown" component={Dropdown}/>
                 <Route  path="/Login" component={Login}/>
                 <Route path="*" component={PageNotFound}/>
             </Switch>
