f3710d03ea77ba05606b82d6adf345bd0486e1de
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-02-04 14:47:27 +0530

    gfdfgdfg

--- src/Components/Common/NavBar.js
+++ src/Components/Common/NavBar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import '../../assets/css/navbar.css'
 // renaming is not allowed. if you want to rename export default
 export function NavBar() {
     return (
-        <div>
-            Deepak
-        </div>
+        <nav>
+            <div className="hamburger">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            <div className="logo-container">
+                <h3 className="logo">Deepak<span>_keshri</span></h3>
+            </div>
+        </nav>
     )
 }
  
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../assets/css/Pages/Home.css'
 export default function Home() {
     return(
-        <>
+        <div className="">
         <h1>Welcome Deepak keshri</h1>
-        </>
+        </div>
     )
 }
