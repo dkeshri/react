5d12bb5b2b6717f57b3eb0aa032747725a185f9c
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-02-04 17:19:54 +0530

    dfdfdf

--- src/Components/Common/Drawer.js
+++ src/Components/Common/Drawer.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,7 +9,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-
+import MenuList from '../Common/MenuList'
 const useStyles = makeStyles({
   list: {
     width: 250,
@@ -32,23 +31,7 @@ export default function Drawer(props) {
       onClick={props.toggleDrawer(false)}
       onKeyDown={props.toggleDrawer(false)}
     >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      <MenuList/>
     </div>
   );
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import MenuList from './Common/MenuList'
 import '../assets/css/Pages/Home.css'
 export default function Home() {
-    return(
+    return (
         <div className="">
-        <h1>Welcome Deepak keshri</h1>
+            <h1>Welcome Deepak keshri</h1>
+            <MenuList></MenuList>
         </div>
     )
 }
