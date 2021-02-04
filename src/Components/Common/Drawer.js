75c5b7bd5cb61648d716dddd226cc72885107b5c
dkeshri <dkeshridev@gmail.com>
2021-02-04 19:07:18 +0530

    dsfsdf

--- src/Components/Common/Drawer.js
+++ src/Components/Common/Drawer.js
@@ -2,13 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuList from '../Common/MenuList'
 const useStyles = makeStyles({
   list: {
@@ -20,7 +13,6 @@ const useStyles = makeStyles({
 });
 
 export default function Drawer(props) {
-  console.log(props.isDrawerOpened);
   const classes = useStyles();
   const list = (anchor) => (
     <div
@@ -28,8 +20,8 @@ export default function Drawer(props) {
         [classes.fullList]: anchor === 'top' || anchor === 'bottom',
       })}
       role="presentation"
-      onClick={props.toggleDrawer(false)}
-      onKeyDown={props.toggleDrawer(false)}
+      // onClick={props.toggleDrawer(false)}
+      // onKeyDown={props.toggleDrawer(false)}
     >
       <MenuList/>
     </div>
