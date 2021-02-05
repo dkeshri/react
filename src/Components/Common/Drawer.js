48f423aec6736551d086c51a03e0a610e25d1303
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-02-05 11:20:10 +0530

    ffffff

--- src/Components/Common/Drawer.js
+++ src/Components/Common/Drawer.js
@@ -2,13 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuList from '../Common/MenuList'
 const useStyles = makeStyles({
   list: {
@@ -28,8 +21,6 @@ export default function Drawer(props) {
         [classes.fullList]: anchor === 'top' || anchor === 'bottom',
       })}
       role="presentation"
-      onClick={props.toggleDrawer(false)}
-      onKeyDown={props.toggleDrawer(false)}
     >
       <MenuList/>
     </div>
