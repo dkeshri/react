389c672561679d1c5d5baae87a1ddb065603fd8e
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-02-05 17:47:49 +0530

    hdkfjhdgf

--- src/Components/Common/MenuList.js
+++ src/Components/Common/MenuList.js
@@ -110,20 +110,6 @@ const getTreeItemsFromData = treeItems => {
         props.menuItemProps.pushMenuItemNodeIds(ids);
       }}
     >
-      {/* <StyledTreeItem nodeId="1" label="Main">
-        <StyledTreeItem nodeId="2" label="Hello" />
-        <StyledTreeItem nodeId="3" label="Subtree with children">
-          <StyledTreeItem nodeId="6" label="Hello" />
-          <StyledTreeItem nodeId="7" label="Sub-subtree with children">
-            <StyledTreeItem nodeId="9" label="Child 1" />
-            <StyledTreeItem nodeId="10" label="Child 2" />
-            <StyledTreeItem nodeId="11" label="Child 3" />
-          </StyledTreeItem>
-          <StyledTreeItem nodeId="8" label="Hello" />
-        </StyledTreeItem>
-        <StyledTreeItem nodeId="4" label="World" />
-        <StyledTreeItem nodeId="5" label="Something something" />
-      </StyledTreeItem> */}
       {getTreeItemsFromData(config.menuItems)}
     </TreeView>
   );
