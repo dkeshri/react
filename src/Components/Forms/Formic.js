daa350f23ee59d43bbb802f9093503b4f08b15c1
dkeshri <dkeshridev@gmail.com>
2021-02-13 23:16:35 +0530

    registration form

--- src/Components/Forms/Formic.js
+++ src/Components/Forms/Formic.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Title from '../Common/Title'
 import style from '../../assets/css/components/forms/Formic.module.css'
 function Formic() {
     console.log(style);
@@ -7,6 +8,7 @@ function Formic() {
         <div className={`${style.container} pageContainer`}>
             <Header title={"Formic Demos"}/>
             <div className={style.contentContainer}>
+                <Title title={"Registration Form"}/>
                 <div className={`${style.box}`}>
                     <div>
                         Registration Form
