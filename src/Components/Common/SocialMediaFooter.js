a5129aa71c15770985d14e2f71f8b37eec78e8c9
dkeshri <dkeshridev@gmail.com>
2021-02-24 12:55:34 +0530

    added footer page

---
+++ src/Components/Common/SocialMediaFooter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/css/SocialMediaFooter.css'
+import SocialMediaIcons from './SocialMediaIcons'
+function SocialMediaFooter() {
+    return (
+        <section className='social-media'>
+            <div>dkeshridev@gmail.com</div>
+            <div>Copyright © Deepak Keshri 2020.</div>
+            <SocialMediaIcons />
+        </section>
+    )
+}
+
+export default SocialMediaFooter
---
+++ src/Components/Common/SocialMediaIcons.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../assets/css/SocialMediaIcons.css'
+function SocialMediaIcons() {
+    return (
+        <div className='social-icons'>
+            <a
+                className='social-icon-link instagram'
+                href='https://www.instagram.com/deepakkeshri0613/'
+                target='_blank'
+                aria-label='Instagram'
+            >
+                <i className='fa fa-instagram' />
+            </a>
+            <a
+                className='social-icon-link youtube'
+                href='https://github.com/dkeshri'
+                target='_blank'
+                aria-label='GitHub'
+            >
+                <i className='fa fa-github-square' />
+            </a>
+            <a
+                className='social-icon-link twitter'
+                href='https://twitter.com/DeepakK18204431'
+                target='_blank'
+                aria-label='Twitter'
+            >
+                <i className='fa fa-twitter' />
+            </a>
+            <a
+                className='social-icon-link twitter'
+                href='https://in.linkedin.com/in/dkeshri'
+                target='_blank'
+                aria-label='LinkedIn'
+            >
+                <i className='fa fa-linkedin' />
+            </a>
+        </div>
+    )
+}
+
+export default SocialMediaIcons
