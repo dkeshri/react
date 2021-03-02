73850a6ed7baeb85524d91903be314343e0bbf5d
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-03-02 15:06:59 +0530

    added client

---
+++ src/data/AppConfig.js
@@ -0,0 +1,9 @@
+const AppConfig = {
+    loginVendor:{
+        google:{
+            clientId:'709977869709-1h9bu161namn0j204mhhvuo1k8b30kom.apps.googleusercontent.com',
+            clientSecret:'J7fdWU8eV3i5x9sq6oUQ64d7'
+        }
+    }
+}
+export { AppConfig }
