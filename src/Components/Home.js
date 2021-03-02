717090678af6a7a58800240e958bafe7414fed73
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-03-02 20:38:32 +0530

    dshsjgd

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import '../assets/css/Pages/Home.css'
+import GoogleLogin from 'react-google-login'
+import {AppConfig} from '../data/AppConfig.js'
 function Home() {
+    const responseGoogle = (res)=>{
+        console.log(res);
+    }
     return (
         <div className="Home-container">
-            <h1>Welcome Deepak keshri</h1>
+            <GoogleLogin
+                clientId={AppConfig.loginVendor.google.clientId}
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     )
 }
