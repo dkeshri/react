847570d774a17ba83bedf7913fdf4c591835dc39
dkeshri <dkeshridev@gmail.com>
2021-03-03 20:42:45 +0530

    sdfsff

--- src/data/AppConfig.js
+++ src/data/AppConfig.js
@@ -1,8 +1,8 @@
 const AppConfig = {
     loginVendor:{
         google:{
-            clientId:'709977869709-1h9bu161namn0j204mhhvuo1k8b30kom.apps.googleusercontent.com',
-            clientSecret:'J7fdWU8eV3i5x9sq6oUQ64d7'
+            clientId:'655542586457-fmhhpf90cqkasd3e30kof5c68l93j6m2.apps.googleusercontent.com',
+            clientSecret:'nOzL4Xg6AW6xzv-i1lONMALh'
         }
     }
 }
