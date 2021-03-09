f4110d8873c892f1389caae6cca8f04b0d142285
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-03-09 13:32:32 +0530

    dsfsdfsf

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,43 @@
 import React from 'react'
-import PageContainer from './Common/PageContainer'
 import style from '../assets/css/AboutUs.module.css'
 const About = () => {
     return (
-        
-            <div className={`box`}>
-                Hello Deepak keshri
+        <>
+            <div className={`${style.box}`}>
+                <div className={style.photo}>
+                    <img
+                        className={style.img}
+                        src={require('../assets/img/profile/Deepak_pic.jpg')} />
+                </div>
+                <div className={style.About}>
+                    <div className={style.header}>
+                        <div className={style.title}>
+                            <h1 className={style.h1}>About</h1>
+                            <span className={style.span}>Me</span>
+                        </div>
+                        <div className={style.line} />
+                        <div className={style.line} />
+                    </div>
+                    <div className={style.desc}>
+                        <div className={style.desc_title}>
+                            Nice to meet you, boss!
+                        </div>
+                        <div className={style.objective_title}>
+                            HERE'S THE DISH ON ME.
+                        </div>
+                        <div className={style.objective}>
+                            <span>My main objective is develop the Software and solve the real life problem and want to implement
+                            that problem into the computing environment.I want to be the reason of success
+                            of company wherever I work.
+                          </span>
+                        </div>
+
+                    </div>
+                </div>
             </div>
 
-       
+        </>
+
     )
 }
 export default About
