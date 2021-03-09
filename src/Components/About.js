77dfdee48d8c4389c4c7c7299ec005446ae0567f
dkeshri <dkeshridev@gmail.com>
2021-03-09 22:44:47 +0530

    gdsjf

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from '../assets/css/AboutUs.module.css'
+import SocialMediaIcons from './Common/SocialMediaIcons'
 const About = () => {
     return (
         <>
@@ -35,7 +36,55 @@ const About = () => {
                     </div>
                 </div>
             </div>
-
+            <div>
+                {/* <div className={style.Contact_heading}>
+                    <h1 className={style.h1}>Contact</h1>
+                    <span className={style.span}>Info</span>
+                </div> */}
+                <div className={` ${style.Contact_container}`}>
+                    <div className={style.Contact}>
+                        <div className={style.Contact_heading}>
+                            <h1 className={style.h1}>Contact</h1>
+                            <span className={style.span}>Info</span>
+                        </div>
+                        <div className={style.row}>
+                            <div className={style.col_left}>
+                                <h5>Name :</h5>
+                            </div>
+                            <div className={style.col_right}>
+                                <span>Deepak keshri</span>
+                            </div>
+                        </div>
+                        <div className={style.row}>
+                            <div className={style.col_left}>
+                                <h5>EmailId :</h5>
+                            </div>
+                            <div className={style.col_right}>
+                                <span>dkeshridev@gmail.com</span>
+                            </div>
+                        </div>
+                        <div className={style.row}>
+                            <div className={style.col_left}>
+                                <h5>Portfolio :</h5>
+                            </div>
+                            <div className={style.col_right}>
+                                <a href="https://dkeshri.github.io/Portfolio/">https://dkeshri.github.io/Portfolio/</a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className={style.Contact_Image}>
+                        <img
+                            className={style.img}
+                            src={require('../assets/img/profile/Dummy.jpg')} />
+                    </div>
+                </div>
+            </div>
+            <div className={style.line_black}></div>
+            <div className={style.line_black}></div>
+            <div className={` ${style.Contact_container}`}>
+                
+            </div>
         </>
 
     )
