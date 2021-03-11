08d58542ad8aa167189032a5fb3a7e3f3d8f2137
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-03-11 14:27:00 +0530

    skills

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -3,11 +3,14 @@ import style from '../assets/css/Skill.module.css'
 const Skill = () => {
     return (
         <div className={style.container}>
-            <div>
-
+            <div className={style.SkillContainer}>
+                <div className={style.box}>
+                    
+                </div>
             </div>
-            <div>
-                
+            <div className={style.SkillTitileContainer}>
+                <h3 className={style.h1}>Skills</h3>
+                <span className={style.span}>that I own</span>
             </div>
         </div>
     )
