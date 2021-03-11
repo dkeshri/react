a355e040e7058e64dd57b435b85348dd37546e76
Deepak Kumar Keshri <dkeshri@india.epayoffice.net>
2021-03-11 13:12:25 +0530

    dsffdfsf

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import style from '../assets/css/AboutUs.module.css'
 import SocialMediaIcons from './Common/SocialMediaIcons'
+import Contact from './Contact'
+import Skill from './Skill'
 const About = () => {
     return (
         <>
@@ -41,52 +43,11 @@ const About = () => {
                     <h1 className={style.h1}>Contact</h1>
                     <span className={style.span}>Info</span>
                 </div> */}
-                <div className={` ${style.Contact_container}`}>
-                    <div className={style.Contact}>
-                        <div className={style.Contact_heading}>
-                            <h1 className={style.h1}>Contact</h1>
-                            <span className={style.span}>Info</span>
-                        </div>
-                        <div className={style.row}>
-                            <div className={style.col_left}>
-                                <h5>Name :</h5>
-                            </div>
-                            <div className={style.col_right}>
-                                <span>Deepak keshri</span>
-                            </div>
-                        </div>
-                        <div className={style.row}>
-                            <div className={style.col_left}>
-                                <h5>EmailId :</h5>
-                            </div>
-                            <div className={style.col_right}>
-                                <span>dkeshridev@gmail.com</span>
-                            </div>
-                        </div>
-                        <div className={style.row}>
-                            <div className={style.col_left}>
-                                <h5>Portfolio :</h5>
-                            </div>
-                            <div className={style.col_right}>
-                                <a target="_blank" href="https://dkeshri.github.io/Portfolio/">https://dkeshri.github.io/Portfolio/</a>
-                            </div>
-                        </div>
-                        <section className={`social-media ${style.socialMedia}`}>
-                            <SocialMediaIcons color="grey" />
-                        </section>
-                    </div>
-                    <div className={style.Contact_Image}>
-                        <img
-                            className={style.img}
-                            src={require('../assets/img/profile/Dummy.jpg')} />
-                    </div>
-                </div>
+                <Contact/>
             </div>
             <div className={style.line_black}></div>
             <div className={style.line_black}></div>
-            <div className={` ${style.Contact_container}`}>
-
-            </div>
+            <Skill/>
         </>
 
     )
---
+++ src/Components/Skill.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from '../assets/css/Skill.module.css'
+const Skill = () => {
+    return (
+        <div className={style.container}>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+export default Skill
